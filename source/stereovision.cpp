3fb4e8173e12df40ab1f260128152738838be8c3
jmruiza <jmruiz@cio.mx>
2013-04-04 17:12:18 -0600

    Test: Detect Harris Corners

--- source/stereovision.cpp
+++ source/stereovision.cpp
@@ -1,8 +1,7 @@
 #include "stereovision.h"
 
-StereoVision::StereoVision()
-{
 
+StereoVision::StereoVision(){
 }
 
 void StereoVision::processImages(){
@@ -16,20 +15,27 @@ void StereoVision::processImages(){
     imageRight.convertTo(tempRight, -1, alpha, beta);
 
     // Detect Harris Corners
-    cv::Mat cornerStrength;
-    cv::cornerHarris(imageLeft,cornerStrength,
+    cv::Mat cornerStrengthLeft, cornerStrengthRight;
+/*    cv::cornerHarris(imageLeft,cornerStrength,
                   3,     // neighborhood size
                      3,     // aperture size
                      0.01); // Harris parameter
+*/
+    cv::cornerHarris(imageLeft, cornerStrengthLeft, 3, 3, 0.01);
+    cv::cornerHarris(imageRight, cornerStrengthRight, 3, 3, 0.01);
 
+    // threshold the corner strengths
+     cv::Mat harrisCornersLeft, harrisCornersRight;
+     double threshold= 0.0001;
+     cv::threshold(cornerStrengthLeft, harrisCornersLeft, threshold, 255, cv::THRESH_BINARY_INV);
+     cv::threshold(cornerStrengthRight, harrisCornersRight, threshold, 255, cv::THRESH_BINARY_INV);
 
+/*
     cv::threshold(tempLeft, imageLeft, 130.0, 255, cv::THRESH_BINARY);
     cv::threshold(tempRight, imageRight, 130.0, 255, cv::THRESH_BINARY);
-
-    //tempLeft.copyTo(imageLeft);
-    //tempRight.copyTo(imageRight);
-
-    // Finding
+*/
+    harrisCornersLeft.copyTo(imageLeft);
+    harrisCornersRight.copyTo(imageRight);
 
 }
 
